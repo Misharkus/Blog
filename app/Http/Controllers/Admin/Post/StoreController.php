caaa228009b6ebbb9f60d12fae1a5c17c1718ca4
Misharkus <mishaelb7@gmail.com>
2023-01-16 13:09:13 +0200

    tags with posts

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -6,17 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
 use Illuminate\Support\Facades\Storage;
+use Exception;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
+        try{
+            $data = $request->validated();
+            $tagIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            $data['preview_image'] = Storage::put('/images', $data['preview_image']);
+            $data['main_image'] = Storage::put('/images', $data['main_image']); 
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tagIds);
+        }catch(Exception $exception) {
+            abort(404);
+        }
         
-        $data = $request->validated();
-        $data['preview_image'] = Storage::put('/images', $data['preview_image']);
-        $data['main_image'] = Storage::put('/images', $data['main_image']); 
-        Post::firstOrCreate($data);
         return redirect()->route('admin.post.index');
     }
-
 }
